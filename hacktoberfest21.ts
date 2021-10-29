d77876bac863d3c65fedad469b666e3b9edd6ed5
mohdshazal <71510463+mohdshazal@users.noreply.github.com>
2021-10-29 17:56:44 +0530

    shazal1

--- hacktoberfest21.ts
+++ hacktoberfest21.ts
@@ -1,3 +1,4 @@
 console.log("hacktober fest 2021") // copy this and paste in new line and add your mail at end of "hacktober fest"
 
 console.log("hacktober fest ajayk@newagesmb.com")
+console.log("hacktober fest muhammedshazal@gmail")
